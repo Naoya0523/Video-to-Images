2e21c5c05c106d6fc5095536f990353c8027ca51
Naoya0523 <nintendonaoya@gmail.com>
2023-04-10 20:40:23 +0900

    first commit

---
+++ Transform.py
@@ -0,0 +1,37 @@
+try:
+    import cv2
+except ImportError:
+    print("'Import Error': Please install openCV library -> !pip install opencv-python")
+
+
+#video name
+videoname = "sample" #please write your video name
+
+video_path = "videos/" + videoname
+
+
+def video_to_images(video_path):
+
+    print('processing...')
+
+    cap = cv2.VideoCapture(video_path)
+    num = 0
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            filepath = "Images/img_" + str(num) + ".jpg"
+            cv2.imwrite(filepath, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("canceled")
+                break
+
+        num += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print('completed!')
+
+if __name__ == 'main':
+    video_to_images(video_path)
